9b8089e7c9307bd7c7a03b2013e4833807c8b0c4
Cahya Rama Vinanza <vinanza77@gmail.com>
2025-07-13 08:44:50 +0700

    Fix: Final attempt at correct nixpacks.toml syntax

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '30f1d14fbf284ef42810561e8d3d220a4bd5efde',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '30f1d14fbf284ef42810561e8d3d220a4bd5efde',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v269',
+            'version' => '269.0.0.0',
+            'reference' => '04aa321ffd7a25205f49dfdb949ad9bb3eae9388',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
